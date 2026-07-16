600f27ce58e47a762b5379ebd220beb770d20706
Raina-Mangat <rainamangat2005@gmail.com>
2026-07-16 23:43:09 +0530

    Remove unused TEAM_COLORS

--- frontend/src/pages/Battle.js
+++ frontend/src/pages/Battle.js
@@ -8,19 +8,7 @@ import axios from 'axios';
 
 const BASE = 'http://localhost:5000';
 
-const TEAM_COLORS = {
-  'Mercedes':        '#00D2BE',
-  'Ferrari':         '#E8002D',
-  'McLaren':         '#FF8000',
-  'Red Bull Racing': '#3671C6',
-  'Alpine':          '#0090FF',
-  'Aston Martin':    '#358C75',
-  'Williams':        '#64C4FF',
-  'Haas F1 Team':    '#B6BABD',
-  'Audi':            '#C0392B',
-  'Cadillac':        '#DDDDDD',
-  'Racing Bulls':    '#6692FF',
-};
+
 
 
 
